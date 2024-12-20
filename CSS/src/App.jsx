209c65d9b868fcb1e9871e61efe18f8e743882ca
Aditya Srivastava <adityasrivastava9406@gmail.com>
2024-12-20 11:38:09 +0530

    Initial commit

---
+++ CSS/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CSSCheatsheet from './CssCheatSheet'
+
+const App = () => {
+  return (
+    <div>
+      <CSSCheatsheet/>
+    </div>
+  )
+}
+
+export default App
